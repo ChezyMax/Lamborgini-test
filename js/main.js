b761860966ea272c7c0c8d2ddc5699922dac5fc0
ChezyMax <104217958+ChezyMax@users.noreply.github.com>
2023-01-21 21:11:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click' , () => {
+    menuMobile.classList.toggle('menu--open');
+});
+
+
 const swiper = new Swiper('.swiper', {
 
     loop: true,
-    slidesPerView: 2,
-    spaceBetween: 20,
     navigation: {
       nextEl: '.swiper-button-right',
       prevEl: '.swiper-button-left',
     },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        }
+    }
   });
 
   let map;
@@ -15,7 +30,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat:37.23198599342622, lng:-8.628183046816766 },
     zoom: 13,
-    styles:[
+    styles: [
         {
             "featureType": "administrative",
             "elementType": "all",
